f8f2a368a4e3d93039122548c7fc3eae1f8d5421
Nguyen Truong Tho <nguyentruongtho.sg@gmail.com>
2014-07-30 16:57:13 +0800

    Test first, expect injection to work for testing

--- src/test/java/com/example/myapp/MyActivityTest.java
+++ src/test/java/com/example/myapp/MyActivityTest.java
@@ -3,6 +3,9 @@ package com.example.myapp;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import javax.inject.Inject;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -12,9 +15,18 @@ import static org.fest.assertions.api.Assertions.assertThat;
 @RunWith(MyRobolectricTestRunner.class)
 //@Config(emulateSdk = 16)
 public class MyActivityTest {
-  @Test public void clickOnButtonShouldShowCorrectMessage() {
-    MyActivity myActivity = Robolectric.setupActivity(MyActivity.class);
+  private MyActivity myActivity;
+  @Inject @FirstLaunch BooleanPreference firstLaunchPreference;
 
+  @Before public void setUp() {
+    myActivity = Robolectric.setupActivity(MyActivity.class);
+  }
+
+  @After public void tearDown() {
+    myActivity = null;
+  }
+
+  @Test public void clickOnButtonShouldShowCorrectMessage() {
     TextView message = (TextView) myActivity.findViewById(R.id.message);
 
     Button clickMeButton = (Button) myActivity.findViewById(R.id.click_me);
@@ -23,5 +35,9 @@ public class MyActivityTest {
     assertThat(message).isNotNull();
     assertThat(message.getVisibility()).isEqualTo(View.VISIBLE);
     assertThat(message.getText()).isEqualTo("Hello Singapore Android Meetup!");
+  }
+
+  @Test public void firstLaunchPreferenceShouldNotBeNull() {
+    assertThat(firstLaunchPreference).isNotNull();
   }
 }
