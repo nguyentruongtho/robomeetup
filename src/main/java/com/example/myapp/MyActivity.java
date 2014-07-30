f3815b693e3f4099235ffa856e6a3284e66aed98
Nguyen Truong Tho <nguyentruongtho.sg@gmail.com>
2014-07-30 14:25:29 +0800

    Write implementation for given behavior

--- src/main/java/com/example/myapp/MyActivity.java
+++ src/main/java/com/example/myapp/MyActivity.java
@@ -2,12 +2,28 @@ package com.example.myapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MyActivity extends Activity {
+  private TextView message;
+  private Button clickMeButton;
+
   /** Called when the activity is first created. */
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+
+    message = (TextView) findViewById(R.id.message);
+
+    clickMeButton = (Button) findViewById(R.id.click_me);
+    clickMeButton.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        message.setVisibility(View.VISIBLE);
+        message.setText("Hello Singapore Android Meetup!");
+      }
+    });
   }
 }
