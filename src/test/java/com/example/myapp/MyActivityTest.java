564247a9d5ea24f89703f9d0c2b7ba54b87ec97f
Nguyen Truong Tho <nguyentruongtho.sg@gmail.com>
2014-07-30 17:04:06 +0800

    Test cases for injected first launch preference

--- src/test/java/com/example/myapp/MyActivityTest.java
+++ src/test/java/com/example/myapp/MyActivityTest.java
@@ -4,8 +4,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import javax.inject.Inject;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -15,18 +13,11 @@ import static org.fest.assertions.api.Assertions.assertThat;
 @RunWith(MyRobolectricTestRunner.class)
 //@Config(emulateSdk = 16)
 public class MyActivityTest {
-  private MyActivity myActivity;
   @Inject @FirstLaunch BooleanPreference firstLaunchPreference;
 
-  @Before public void setUp() {
-    myActivity = Robolectric.setupActivity(MyActivity.class);
-  }
-
-  @After public void tearDown() {
-    myActivity = null;
-  }
-
   @Test public void clickOnButtonShouldShowCorrectMessage() {
+    MyActivity myActivity = Robolectric.setupActivity(MyActivity.class);
+
     TextView message = (TextView) myActivity.findViewById(R.id.message);
 
     Button clickMeButton = (Button) myActivity.findViewById(R.id.click_me);
@@ -34,10 +25,32 @@ public class MyActivityTest {
 
     assertThat(message).isNotNull();
     assertThat(message.getVisibility()).isEqualTo(View.VISIBLE);
-    assertThat(message.getText()).isEqualTo("Hello Singapore Android Meetup!");
+    assertThat(message.getText().toString()).isEqualTo("Hello Singapore Android Meetup!");
   }
 
   @Test public void firstLaunchPreferenceShouldNotBeNull() {
     assertThat(firstLaunchPreference).isNotNull();
+  }
+
+  @Test public void firstLaunchMessageShouldBeShownOnFirstTimeLaunchingTheApp() {
+    firstLaunchPreference.set(true);
+
+    MyActivity myActivity = Robolectric.setupActivity(MyActivity.class);
+    TextView firstLaunchText = (TextView) myActivity.findViewById(R.id.first_launch_message);
+
+    assertThat(firstLaunchText).isNotNull();
+    assertThat(firstLaunchText.getVisibility()).isEqualTo(View.VISIBLE);
+    assertThat(firstLaunchText.getText().toString())
+        .isEqualTo("You can see me only in the first time launching this app :)");
+  }
+
+  @Test public void secondLaunchOnwardShouldNotShowFirstLaunchMessage() {
+    firstLaunchPreference.set(false);
+
+    MyActivity myActivity = Robolectric.setupActivity(MyActivity.class);
+    TextView firstLaunchText = (TextView) myActivity.findViewById(R.id.first_launch_message);
+    
+    assertThat(firstLaunchText).isNotNull();
+    assertThat(firstLaunchText.getVisibility()).isEqualTo(View.GONE);
   }
 }
